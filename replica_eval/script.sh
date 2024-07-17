34897c0986e29130784487ef3484697a505a96e5
WonGyeom <90492809+EoNjesajo@users.noreply.github.com>
2024-07-17 20:02:37 +0900

    re

---
+++ replica_eval/script.sh
@@ -0,0 +1,27 @@
+# python evaluate_single_scene.py --input_mesh result/RICO/replica/scan1.ply --scan_id 1 --output_dir rico/replica1
+# python evaluate_single_scene.py --input_mesh result/RICO/replica/scan2.ply --scan_id 2 --output_dir rico/replica2
+# python evaluate_single_scene.py --input_mesh result/RICO/replica/scan3.ply --scan_id 3 --output_dir rico/replica3
+# python evaluate_single_scene.py --input_mesh result/RICO/replica/scan4.ply --scan_id 4 --output_dir rico/replica4
+# python evaluate_single_scene.py --input_mesh result/RICO/replica/scan5.ply --scan_id 5 --output_dir rico/replica5
+# # python evaluate_single_scene.py --input_mesh result/RICO/replica/scan6.ply --scan_id 6 --output_dir rico/replica6
+# # python evaluate_single_scene.py --input_mesh result/RICO/replica/scan7.ply --scan_id 7 --output_dir rico/replica7
+# # python evaluate_single_scene.py --input_mesh result/RICO/replica/scan8.ply --scan_id 8 --output_dir rico/replica8
+
+
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan1.ply --scan_id 1 --output_dir objsdf/replica1
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan2.ply --scan_id 2 --output_dir objsdf/replica2
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan3.ply --scan_id 3 --output_dir objsdf/replica3
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan4.ply --scan_id 4 --output_dir objsdf/replica4
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan5.ply --scan_id 5 --output_dir objsdf/replica5
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan6.ply --scan_id 6 --output_dir objsdf/replica6
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan7.ply --scan_id 7 --output_dir objsdf/replica7
+# python evaluate_single_scene.py --input_mesh result/objsdf/replica/scan8.ply --scan_id 8 --output_dir objsdf/replica8
+
+# python evaluate_single_scene.py --input_mesh result/replica1_100.ply --scan_id 1 --output_dir replica1/100
+# python evaluate_single_scene.py --input_mesh result/replica2_100.ply --scan_id 2 --output_dir replica2/100
+# python evaluate_single_scene.py --input_mesh result/replica3_100.ply --scan_id 3 --output_dir replica3/100
+# python evaluate_single_scene.py --input_mesh result/replica4_100.ply --scan_id 4 --output_dir replica4/100
+# python evaluate_single_scene.py --input_mesh result/replica5_100.ply --scan_id 5 --output_dir replica5/100
+# python evaluate_single_scene.py --input_mesh result/replica6_100.ply --scan_id 6 --output_dir replica6/100
+# python evaluate_single_scene.py --input_mesh result/replica7_100.ply --scan_id 7 --output_dir replica7/100
+# python evaluate_single_scene.py --input_mesh result/replica8_100.ply --scan_id 8 --output_dir replica8/100
